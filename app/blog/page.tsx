ddf7b849b4158686d9d09a3dbe7485e9cb71f621
l11040 <113540683+l11040@users.noreply.github.com>
2024-06-09 00:48:45 +0900

    Update

---
+++ app/blog/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import useGetNotionTable from "@/api/useGetNotionTable";
+import NotionPosts from "@/components/blog/NotionPosts";
+import ContentsLayout from "@/layouts/ContentsLayout";
+
+export default function Blog() {
+  const { data: blogPosts } = useGetNotionTable(
+    "3723371501ca4ebba16f4ca8f65c5785",
+  );
+  return (
+    <ContentsLayout title="Blog">
+      <NotionPosts blogPosts={blogPosts} />
+    </ContentsLayout>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,15 @@ import DefaultLayout from "@/layouts/DefaultLayout";
 
 export default function Home() {
   return (
-    <DefaultLayout>
-      <div className="flex flex-col gap-8">
-        <ContentsLayout title="Career">
-          {/* <CareerList /> */}
-          <div />
-        </ContentsLayout>
-        <ContentsLayout title="Blog">
-          {/* <NotionPosts blogPosts={blogPosts} /> */}
-          <div />
-        </ContentsLayout>
-      </div>
-    </DefaultLayout>
+    <div className="flex flex-col gap-8">
+      <ContentsLayout title="Career">
+        {/* <CareerList /> */}
+        <div />
+      </ContentsLayout>
+      <ContentsLayout title="Blog">
+        {/* <NotionPosts blogPosts={blogPosts} /> */}
+        <div />
+      </ContentsLayout>
+    </div>
   );
 }
